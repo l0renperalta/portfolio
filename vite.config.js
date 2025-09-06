ba58ef9e9273d76192e623d78e462a1d0de64c42
l0ren <stinpc@hotmail.com>
2025-09-06 11:44:06 -0500

    fix:vite base param

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://l0renperalta.github.io/portfolio/',
+  base: './', // Esta línea es importante
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 });
